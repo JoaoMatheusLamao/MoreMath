12a8555402e0952fe6fcb75baa3b666565286d78
Vitorhfc <105317266+Vitorhfc@users.noreply.github.com>
2022-08-19 09:02:16 -0300

    Update css cadastro
    Arrumei proporção das divs;
    Comecei a trabalhar no login.

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 require_once("config.php");
-
 if (isset($_POST['email']) && isset($_POST['senha']) && !empty($_POST['email']) && !empty($_POST['senha'])) {
     $email = Usuario::limpaPost($_POST['email']);
     $senha = Usuario::limpaPost($_POST['senha']);
@@ -21,22 +20,30 @@ if (isset($_POST['email']) && isset($_POST['senha']) && !empty($_POST['email']) 
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MoreMath</title>
-    <link rel="stylesheet" type="text/css" href="css/style_login.css">
+    <link rel="stylesheet" type="text/css" href="css/log_style.css">
 </head>
 
 <body>
-    <form method="post">
-        <div class="main">
-            .cadastroz
-        </div>
+    <div class="main">
+        <div class="quadro">
+            <div class="titulo">
+                <p id="titulo_texto">Faça Login ♥</p>
 
-        <label for="email">Email:</label>
-        <input type="email" name="email" id="email" placeholder="Digite seu melhor email!"><br>
-        <label for="email">Senha:</label>
-        <input type="password" name="senha" id="senha"><br>
-        <button type="submit">Cadastrar</button>
-    </form>
-    <p>Ainda não tem um cadastro?<a href="cadastrar.php">Cadastre-se</a></p>
+            </div>
+            <div class="entrada_login">
+                <form method="post">
+                    <label for="email">Email:</label>
+                    <input type="email" name="email" id="email" placeholder="Digite seu melhor email!"><br>
+                    <label for="email">Senha:</label>
+                    <input type="password" name="senha" id="senha"><br>
+                    <button type="submit">Cadastrar</button>
+                </form> 
+            </div>
+            <div class="cadastrar_se">
+                <p>Ainda não tem um cadastro?<a href="cadastrar.php">Cadastre-se</a></p>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
