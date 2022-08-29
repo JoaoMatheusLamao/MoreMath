b61e2150ffa566ae6ca0a2931a40c2ed4595f32f
Vitorhfc <vitorhfcampos@gmail.com>
2022-08-29 17:25:45 -0300

    atualização 29/08/2022

--- restrito/index.php
+++ restrito/index.php
@@ -19,23 +19,26 @@ $usuario = new UsuarioOk();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css "href="css/style_livrinhos.css">
-    <link rel="shortcut icon" href="img/coruja.png" type="image/x-icon">
+    <link rel="stylesheet" type="text/css "href="css/livrinhos_style.css">
+    <link rel="shortcut icon" href="css/img/Coruja.png" type="image/x-icon">
     <title>Document</title>
 </head>
 <body>
-    <header>
-        <!-- cabeçario com a logo, perfil, e nome da pagina  -->
-        <button> </button>
+<!-- cabeçario com a logo, perfil, e nome da pagina  -->
+    <header>        
+        <div class="pag_nom">Conteúdos</div>
+        <img src="css/img/Coruja.png">
+        <button><img src="css/img/Usuario botão.png"></button>
+
     </header>
-    <div class="main">
-        <!-- livros -->
+<!-- livros -->
+    <main>  
         <div class="estante">
-            <div class="livro_soma"><a href="conteudos/soma.php"><img src="img/livro_1.png" alt="Livro de adição"></a> </div>
-            <div class="livro_subtracao"><a href="conteudos/subtracao.php"><img src="img/livro_2.png" alt="Livro de subtração"></a> </div>
-            <div class="livro_multiplicao"><a href="conteudos/soma.php"><img src="img/livro_3.png" alt="Livro de multiplicação"></a> </div>
-            <div class="livro_divisao"><a href="conteudos/multiplicacao.php"><img src="img/livro_4.png" alt="Livro de divisão"></a> </div>
+            <div class="livro_soma"><a href="conteudos/soma.php"><img src="css/img/livro_1.png" alt="Livro de adição"></a> </div>
+            <div class="livro_subtracao"><a href="conteudos/subtracao.php"><img src="css/img/livro_2.png" alt="Livro de subtração"></a> </div>
+            <div class="livro_multiplicao"><a href="conteudos/soma.php"><img src="css/img/livro_3.png" alt="Livro de multiplicação"></a> </div>
+            <div class="livro_divisao"><a href="conteudos/multiplicacao.php"><img src="css/img/livro_4.png" alt="Livro de divisão"></a> </div>
         </div>
-    </div>
+    </main>
 </body>
 </html> 
