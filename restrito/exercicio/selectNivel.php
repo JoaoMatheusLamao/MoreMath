c39bb0b141c276bbb541347c54aa6265f4a5999d
Vitorhfc <vitorhfcampos@gmail.com>
2022-09-13 23:01:04 -0300

    mudanças index(livrinhos) e selectNivel

--- restrito/exercicio/selectNivel.php
+++ restrito/exercicio/selectNivel.php
@@ -10,27 +10,27 @@
 
     <link rel="shortcut icon" href="css/img/Coruja.png" type="image/x-icon">
 
-    <link rel="stylesheet" href="css/style_menu_exercicios.css">
+    <link rel="stylesheet" href="css/style_menu_ex.css">
 
     <title>Escolha um nível</title>
 </head>
 
 <body>
     <main>
-        <div class="quadro_global">
-            <div class="quadro" id="Quadro_1">
-                <div class="select_nivel" id="esquerda">
-                    <p>Nível:</p>
-                    <form action="php/callFx.php">
-                        <input type="submit" value="Fixação">
-                    </form>  
-                </div>
+        <div class="quadro" id="Quadro_1">
+            <div class="select_nivel" id="esquerda">
+                <p>Nível :</p>
+                <form action="php/callFx.php">
+                    <input type="submit" value="Fixação">
+                </form>  
             </div>
-            <div class="quadro" id="Quadro_2">
-                <div class="select_nivel" id="direita">
-                    <p>Nível:</p>
-                    <a href="exercicioAp.php"><p>Aprofundamento</p></a>
-                </div>
+        </div>
+        <div class="quadro" id="Quadro_2">
+            <div class="select_nivel" id="direita">
+                <p>Nível :</p>
+                <form action="php/callFx.php">
+                    <input type="submit" value="Aprofundamento">
+                </form>  
             </div>
         </div>
     </main>
